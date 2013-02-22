e5a88da92d27b9972b54063927351b86882845c3
Dan Klotz <danferth@gmail.com>
2013-02-22 14:09:01 -0500

    removed unneeded files and adjusted footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
 
 <?php
-echo "<p>FUNZONE | &copy; ";  date("y");  echo "<a href =\"http://www.danferth.com\">danferth.com</a> an <a href=\"http://www.akillustrations.com\" target=\"_blank\">AKillustrations</a> production</p>";
-
-?>
+echo "<p>FUNZONE | &copy; ";  date("y"); ?>
