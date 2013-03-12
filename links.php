5bc05cdaa6d2db7095aedc6031bfacff2e27926a
Dan Klotz <danferth@gmail.com>
2013-03-12 14:58:32 -0400

    rename reset to normalize, added BG images, adjusted layout

--- links.php
+++ links.php
@@ -4,7 +4,7 @@
   <!--google fonts-->
   <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
   <!--reset-->
-  <link rel="stylesheet" href="css/reset.css" />
+  <link rel="stylesheet" href="css/normalize.css" />
   <!--main style-->
   <link rel="stylesheet" href="css/style.css">
 <!--page spacific stylesheet-->
