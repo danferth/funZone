e5cd3918dc1f5d31d077c46e013495520d2dcede
Dan Klotz <danferth@gmail.com>
2013-03-12 14:43:46 -0400

    fixed home.css bug

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
 $title = "funzone";
 $description = "this is a fun website";
-$page_style = "home.css";
+$page_style = "";
 
 $header='content/home_header.html';
 $main='content/home_content.html';
